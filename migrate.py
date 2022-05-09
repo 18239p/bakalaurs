dbeb1ff6d93d4c0f6ccfa7f06b00e493923dba3d
18239p <18239p@github.com>
2022-05-09 12:20:25 +0300

    iesākts argparse

--- migrate.py
+++ migrate.py
@@ -2,3 +2,7 @@
 #Pseudocode : python -> spawn bash shell -> virt-v2v [args] -> qemu-img [args] -> scp [args]
 #
 import argparse
+
+def main():
+	parser = argparse.ArgumentParser(description="Migrācijas prototips")
+	
