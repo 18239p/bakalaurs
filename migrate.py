0546f075e2f0e5bc99f54db09052e762e004d52e
18239p <18239p@github.com>
2022-05-09 13:09:30 +0300

    pievienots pseudokods

--- migrate.py
+++ migrate.py
@@ -10,14 +10,19 @@ import argparse
 
 #Pseudocode
 #def ssh
-#subprocess.run('[scp, VMWare_Input, VMWare_Password, selected_VM, vmware root dir')
+#subprocess.run('[scp, VMWare_Input, VMWare_Password, selected_VM, vmware root dir',shell=True, capture_output=True,text=True)
+
+#pseudocode
+#qemu-img -f qcow2 -O vmdk selected_VM.qcow2 selected_VM.vmdk
+#subprocess.run('[qemu-img,  selected_VM.qcow2, -O selected_VM(.vmdk)]',shell=True,capture_output=True,text=True)
+
 def main():
 	parser = argparse.ArgumentParser(description="Migrācijas prototips")
 	parser.add_argument('-i',dest='Xen_input',  help="Xen servera adrese")
 	parser.add_argument('-p',dest='password',  help="Xen servera parole")
 	parser.add_argument('-vm',dest='selected_VM', help="Xen servera Virtuālās mašīnas nosaukums")
 	parser.add_argument('-P',dest='VMWare_Input',  help="VMWare servera adrese")
-	parser.add_argument('-vP',dest='VMWare_Password', help="VMWare servera parole")	
+	parser.add_argument('-vP',dest='VMWare_Password', help="VMWare servera parole")
 	parser.add_argument('-O',dest='VM_output_name',  help="Pārtaisītās KVM virtuālās mašīnas formāta nosaukums")
 	args = parser.parse_args()
 	xen_input      = args.Xen_input
