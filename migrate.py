f643a43dddde7e9986f29cd8a5f26149fac79be0
18239p <18239p@github.com>
2022-05-09 12:49:38 +0300

    main paplašinājums

--- migrate.py
+++ migrate.py
@@ -11,6 +11,8 @@ def main():
 	parser.add_argument('-P',dest='VMWare_Input',  help="VMWare servera adrese")
 	parser.add_argument('-O',dest='VM_output_name',  help="Pārtaisītās KVM virtuālās mašīnas formāta nosaukums")
 	args = parser.parse_args()
-	xen_input   = args.Xen_input
-	password    = args.password
-	selected_VM = args.selected_VM 
+	xen_input    = args.Xen_input
+	password     = args.password
+	selected_VM  = args.selected_VM 
+	VMWare_Input = args.VMWare_Input
+	 
