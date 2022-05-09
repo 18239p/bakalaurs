1a66e124e8692243f8ccad1d6851401a895f7cfb
18239p <18239p@github.com>
2022-05-09 13:43:23 +0300

    main pretty-code

--- migrate.py
+++ migrate.py
@@ -19,6 +19,7 @@ def scp (VMWare_Input,VMWare_Password,selected_VM):
 def qemu_img(selected_VM):
 	subprocess.run(['qemu-img', '-f qcow2 -O vmdk', selected_VM+'.qcow2', selected_VM+'.vmdk'],shell=True,capture_output=True,text=True)
 
+#ienākošie mainīgie
 parser = argparse.ArgumentParser(description="Migrācijas prototips")
 parser.add_argument('-i',dest='Xen_input',  help="Xen servera adrese")
 parser.add_argument('-p',dest='Xen_password',  help="Xen servera parole")
@@ -27,6 +28,7 @@ parser.add_argument('-P',dest='VMWare_Input',  help="VMWare servera adrese")
 parser.add_argument('-vP',dest='VMWare_Password', help="VMWare servera parole")
 parser.add_argument('-O',dest='VM_output_name',  help="Pārtaisītās KVM virtuālās mašīnas formāta nosaukums")
 
+#ienākošo mainīgo piešķiršana/galvenā loģika
 if __name__ == '__main__':
 	args = parser.parse_args()
 	xen_input      = args.Xen_input
