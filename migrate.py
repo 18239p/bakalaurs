a40769d57d8a15d4c10117a8c3f36236392c35dd
18239p <18239p@github.com>
2022-05-09 14:22:02 +0300

    funkciju papildinājums

--- migrate.py
+++ migrate.py
@@ -6,20 +6,22 @@ import subprocess
 #Pseudocode:
 # virt-v2v -ic 'xen+ssh://root@xen.example.com' -ip password Guest_name
 def virt_v2v(xen_input,xen_password,selected_VM):
-	subprocess.run(['virt-v2v', xen_input, xen_password, selected_VM], shell=True, capture_output=True,text=True)
+	virt_result=subprocess.run(['virt-v2v', xen_input, xen_password, selected_VM], shell=True, capture_output=True,text=True)
+	return virt_result.stdout
 #
 
 #Pseudocode:
 # scp selected_VM.vmdk root@vm.wa.re.address /sshrootdir/ 
 def scp (VMWare_Input,VMWare_Password,selected_VM):
-	subprocess.run(['scp', VMWare_Input, VMWare_Password, selected_VM], '/',shell=True, capture_output=True,text=True)
+	scp_result=subprocess.run(['scp', VMWare_Input, VMWare_Password, selected_VM], '/',shell=True, capture_output=True,text=True)
+	return scp_result.stdout
 #
 
 #Pseudocode:
-#qemu-img -f qcow2 -O vmdk selected_VM.qcow2 selected_VM.vmdk
+# qemu-img -f qcow2 -O vmdk selected_VM.qcow2 selected_VM.vmdk
 def qemu_img(selected_VM):
-	subprocess.run(['qemu-img', '-f qcow2 -O vmdk', selected_VM+'.qcow2', selected_VM+'.vmdk'],shell=True,capture_output=True,text=True)
-
+	Qemu_result=subprocess.run(['qemu-img', '-f qcow2 -O vmdk', selected_VM+'.qcow2', selected_VM+'.vmdk'],shell=True,capture_output=True,text=True)
+	return Qemu_result.stdout
 #ienākošie mainīgie
 parser = argparse.ArgumentParser(description="Migrācijas prototips")
 parser.add_argument('-i',dest='Xen_input',  help="Xen servera adrese")
@@ -37,4 +39,10 @@ if __name__ == '__main__':
 	selected_VM    = args.selected_VM
 	VMWare_Input   = args.VMWare_Input
 	VM_output_name = args.VM_output_name
-
+	
+	#1.
+	#virt-v2v
+	#2.
+	#qemu_img
+	#3.
+	#scp
