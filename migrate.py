77dac0c9acf28b4abe0a7432ff304049405e5772
18239p <18239p@github.com>
2022-05-09 14:56:36 +0300

    pievienota funkcionalitāte

--- migrate.py
+++ migrate.py
@@ -7,8 +7,8 @@ import argparse
 import subprocess
 #Pseudocode:
 # virt-v2v -ic 'xen+ssh://root@xen.example.com' -ip password Guest_name
-def virt_v2v(xen_input,xen_password,selected_VM):
-	virt_result=subprocess.run(['virt-v2v', xen_input, xen_password, selected_VM], shell=True, capture_output=True,text=True)
+def virt_v2v(Xen_input,Xen_password,selected_VM):
+	virt_result=subprocess.run(['virt-v2v', Xen_input, Xen_password, selected_VM], shell=True, capture_output=True,text=True)
 	return virt_result.stdout
 #
 
@@ -26,24 +26,29 @@ def qemu_img(selected_VM):
 	return Qemu_result.stdout
 #ienākošie mainīgie
 parser = argparse.ArgumentParser(description="Migrācijas prototips")
-parser.add_argument('-i',dest='Xen_input',  help="Xen servera adrese")
-parser.add_argument('-p',dest='Xen_password',  help="Xen servera parole")
-parser.add_argument('-vm',dest='selected_VM', help="Xen servera Virtuālās mašīnas nosaukums")
-parser.add_argument('-P',dest='VMWare_Input',  help="VMWare servera adrese")
-parser.add_argument('-vP',dest='VMWare_Password', help="VMWare servera parole")
-parser.add_argument('-O',dest='VM_output_name',  help="Pārtaisītās KVM virtuālās mašīnas formāta nosaukums")
+parser.add_argument('-i',dest='Xen_input',  help="Xen servera adrese", required=True)
+parser.add_argument('-p',dest='Xen_password',  help="Xen servera parole", required=True)
+parser.add_argument('-vm',dest='selected_VM', help="Xen servera Virtuālās mašīnas nosaukums", required=True)
+parser.add_argument('-P',dest='VMWare_Input',  help="VMWare servera adrese", required=True)
+parser.add_argument('-vP',dest='VMWare_Password', help="VMWare servera parole", required=True)
+parser.add_argument('-O',dest='VM_output_name',  help="Pārtaisītās KVM virtuālās mašīnas formāta nosaukums", required=True)
 
-#ienākošo mainīgo piešķiršana/galvenā loģika
-if __name__ == '__main__':
+#ienākošo mainīgo piešķiršana/main logic
+def main():
 	args = parser.parse_args()
-	xen_input      = args.Xen_input
-	xen_password   = args.Xen_password
+	Xen_input      = args.Xen_input
+	Xen_password   = args.Xen_password
 	selected_VM    = args.selected_VM
 	VMWare_Input   = args.VMWare_Input
 	VM_output_name = args.VM_output_name
-	
+if __name__ == '__main__':
+	main()
 	#1.
 	#virt-v2v
+	virt_v2v(Xen_input,Xen_password,selected_VM)
+	print(virt_v2v)
+	qemu_img()
+	print(qemu_img)
 	#2.
 	#qemu_img
 	#3.
