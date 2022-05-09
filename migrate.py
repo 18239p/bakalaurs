94c2c92d1d116a1699bfd362e4b6fe87d79f2f16
18239p <18239p@github.com>
2022-05-09 12:30:08 +0300

    ievades saglabāšana

--- migrate.py
+++ migrate.py
@@ -5,8 +5,9 @@ import argparse
 
 def main():
 	parser = argparse.ArgumentParser(description="Migrācijas prototips")
-	parser.add_argument('-i', help="Xen servera adrese")
-	parser.add_argument('-p', help="Xen servera parole")
+	parser.add_argument('-i',  help="Xen servera adrese")
+	parser.add_argument('-p',  help="Xen servera parole")
 	parser.add_argument('-vm', help="Xen servera Virtuālās mašīnas nosaukums")
-	parser.add_argument('-P',help="VMWare servera adrese")
-	parser.add_argument('-O', help="Pārtaisītās KVM virtuālās mašīnas formāta nosaukums")
+	parser.add_argument('-P',  help="VMWare servera adrese")
+	parser.add_argument('-O',  help="Pārtaisītās KVM virtuālās mašīnas formāta nosaukums")
+	args = parser.parse_args()
